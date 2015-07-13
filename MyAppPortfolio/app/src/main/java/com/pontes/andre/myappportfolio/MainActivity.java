99a1d8df2c7b651581778cf825985e7182f66d68
Andre Pontes Melo <andrepontes@gmail.com>
2015-07-13 11:22:41 -0300

    * Using LinearLayout main activity  * using dp for units except for text size.  * Using Styles  * using onClick event.  * Removing menu.

--- MyAppPortfolio/app/src/main/java/com/pontes/andre/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/pontes/andre/myappportfolio/MainActivity.java
@@ -11,88 +11,42 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void showToast(View view) {
+        switch (view.getId()) {
+            case R.id.button_movies:
+                displayToast("This button will launch my movie app!");
+                break;
+
+            case R.id.button_scores:
+                displayToast("This button will launch my scores app!");
+                break;
+
+            case R.id.button_library:
+                displayToast("This button will launch my library app!");
+                break;
+
+            case R.id.button_build:
+                displayToast("This button will launch my build app!");
+                break;
+
+            case R.id.button_xyz:
+                displayToast("This button will launch my xyz app!");
+                break;
+
+            case R.id.button_own:
+                displayToast("This button will launch my own app!");
+                break;
+
+        }
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button buttonSpotify = (Button) findViewById(R.id.button_spotify);
-        buttonSpotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my spotify app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final Button buttonScores = (Button) findViewById(R.id.button_scores);
-        buttonScores.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my scores app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final Button buttonLibrary = (Button) findViewById(R.id.button_library);
-        buttonLibrary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my library app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final Button buttonBuild = (Button) findViewById(R.id.button_build);
-        buttonBuild.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my build app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final Button buttonXYZ = (Button) findViewById(R.id.button_xyz);
-        buttonXYZ.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my XYZ app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final Button buttonOwn = (Button) findViewById(R.id.button_own);
-        buttonOwn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "This button will launch my own app!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
